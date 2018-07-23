095d1ed363fabfda41f9d4d926f4d1e59d94856a
Lesh <11671272+derLesh@users.noreply.github.com>
2018-07-23 23:57:08 +0200

    Added json problem

--- discord-webhook.lua
+++ discord-webhook.lua
@@ -1,14 +1,47 @@
 AddCSLuaFile()
 
-json = require("json")
+j = require("json")
 
 webhookURL = "https://discordapp.com/api/webhooks/XXX"
 local DiscordWebhook = DiscordWebhook or {}
 
-hook.Add( "PlayerSay", "SendMsg", function( ply, text )
-    t_post = {
-        content = text,
+local addonName = "[Discord-Webhook]"
+
+if SERVER then
+    print(addonName .. " loaded")
+end
+
+hook.Add( "FirstTime", "Init", function()
+    CreateConVar("sv_webhook", "0", FCVAR_PROTECTED, "Get your Discord channel webhook link and set the link as value")
+end )
+
+function DiscordWebhook:CheckRole( ply )
+    if ply:IsRole(ROLE_TRAITOR) then
+        role = "Traitor > "
+        elseif ply:IsRole(ROLE_DETECTIVE) then
+            role = "Detective > "
+        else 
+            role = ""
+    end
+end
+
+function DiscordWebhook:BuildMessage( ply, text )
+    msg = util:TableToJSON( {
+        embeds = { 
+            description = text, 
+            author = { 
+                name = ply:Nick()
+            }, 
+        }, 
+    } )
+    return msg
+end
+
+hook.Add( "PlayerSay", "SendMsg", function( ply, msg )
+    fMsg = {
+        content = BuildMessage:msg(ply, msg),
         username = "TTT-Log",
-    } 
+    }
     http.Post(webhookURL, t_post)
+    print( "Sucsess" )
 end )
